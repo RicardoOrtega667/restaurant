0ee13e8c5e7938ced95fd0ff365c619d865ae0fe
RicardoOrtega667 <ricardo_or93@outlook.com>
2023-01-07 21:24:54 -0600

    tercer commit

--- store/src/index.js
+++ store/src/index.js
@@ -6,6 +6,7 @@ import BasicExample from "./BasicExample";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Main from "./pages/main";
 import App from "./App";
+import Footer from "./pages/footer";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -14,6 +15,7 @@ root.render(
     <BasicExample />
     <Main />
     <App />
+    <Footer />
     
     </React.StrictMode>
 );
---
+++ store/src/pages/footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="reservations">
+        <h1>Reserva con nosotros!</h1>
+      <input type="text"></input>
+      <label>Correo</label>
+      <input type="text"></input>
+      <label>Nombre de la reservacion</label>
+      <input type="text"></input>
+      <label>Numero de Personas</label>
+    </div>
+  );
+}
+
+export default Footer;
